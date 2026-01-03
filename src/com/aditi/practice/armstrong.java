c20fefc90779aa974aa98baf14df67d22fee2525
Aditi gupta <aditigupta.ag226@gmail.com>
2026-01-03 21:42:38 +0530

    armstrong number

---
+++ src/com/aditi/practice/armstrong.java
@@ -0,0 +1,31 @@
+package com.aditi.practice;
+
+import java.util.Scanner;
+
+public class armstrong {
+    public static void main(String[] args) {
+        //tell given number is armstrong or not
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i = 100; i < 1000 ; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i + " ");
+            }
+        }
+
+        System.out.println(isArmstrong(n));
+    }
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while(n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem*rem*rem;
+        }
+        return sum == original;
+    }
+
+}
