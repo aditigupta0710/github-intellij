9b5015b69203f2c15d87491703d1bbf22034fc55
Aditi gupta <aditigupta.ag226@gmail.com>
2026-01-03 15:17:23 +0530

    questions of prime

--- src/com/aditi/practice/overloading.java
+++ src/com/aditi/practice/overloading.java
@@ -1,4 +1,16 @@
 package com.aditi.practice;
 
 public class overloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Aditi Gupta");
+    }
+    static void fun(int a){
+        System.out.println("first one");
+        System.out.println(a);
+    }
+    static void fun(String name) {
+        System.out.println("second one");
+        System.out.println(name);
+    }
 }
--- src/com/aditi/practice/variable_arguments.java
+++ src/com/aditi/practice/variable_arguments.java
@@ -1,4 +1,16 @@
 package com.aditi.practice;
 
-public class variable_arguments {
+import java.util.Arrays;
+
+public class variable_arguments{
+    public static void main(String[] args) {
+        fun(76,43,54,3,3,2,22,2,2,32);
+        multiple(45,65,"Aditi", "ayush");
+    }
+    static void fun(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+    static void multiple(int a, int b, String ...v) {
+
+    }
 }
